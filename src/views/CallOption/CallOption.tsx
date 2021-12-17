0df0631923441855373beb8f6e914dfbeb690632
goburhan <goburhann@gmail.com>
2021-12-18 02:06:20 +0300

    final versionn

--- src/views/CallOption/CallOption.tsx
+++ src/views/CallOption/CallOption.tsx
@@ -148,7 +148,7 @@ const CallOption: React.FC<FarmsProps> = (farmsProps) => {
   const letAllowance = async () => {
     await busdContract.methods
       .approve(
-        account,
+        lockedSale,
         '115792089237316195423570985008687907853269984665640564039457584007913129639935',
       )
       .send({ from: account })
